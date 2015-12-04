4b3bf3a0ad1df15a1ad0a6f79146ed881b048d99
minh93 <phamducminh.bri@gmail.com>
2015-12-04 14:06:00 +0700

    refactor link in home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,9 @@ class StaticPagesController < ApplicationController
   before_action :logged_in_user, only: :home
   
   def home
-    @activities = current_user.activities.paginate page: params[:page],
+    @activities = current_user.activities.reverse_order.paginate page: params[:page],
       per_page: 30
+    @activities
     respond_to :html, :js
   end
 
--- app/models/user_goal.rb
+++ app/models/user_goal.rb
@@ -1,3 +1,10 @@
 class UserGoal < ActiveRecord::Base
+  include ActivityLog
+  after_create :create_goal_log
   belongs_to :user
+  has_many :activities, as: :targetable
+
+  def create_goal_log
+    create_log self, self.user_id, Settings.activity_type.create_goal
+  end
 end
